89a0d9ddd53d66b76260e3301efbee0aa6f2d5b7
zeio <zeionara@gmail.com>
2022-12-24 00:03:41 +0300

    Implemented a simple ingestion process which drops stop word tokens

--- ahri/ingest.r
+++ ahri/ingest.r
@@ -1,3 +1,35 @@
-comments <- read.csv(file = 'assets/data/comments.csv')
+library(tokenizers)
+library(dplyr)
 
-head(comments)
+comments <- read.csv(file = "assets/data/comments.csv")
+
+stop_words <- c("the", "on", "that")
+
+# print(c(sentiments[1], c("foo", "bar")))
+
+# tokens <-
+#     comments[, "Comment"] %>%
+#         head(2) %>%
+#         tokenize_words()
+
+# comment <- tokens[[1]]
+
+# comment_without_stop_words <- comment[!sapply(comment, function(x) x %in% stop_words)]
+
+filtered_tokens <- comments[, "Comment"] %>%
+    tokenize_words() %>%
+    lapply(function(text) text[!sapply(text, function(x) x %in% stop_words)])
+
+sentiments <- comments[, "Sentiment"]
+
+dir.create("assets/ingested", recursive = TRUE)
+
+save(filtered_tokens, file = "assets/ingested/tokens.rda")
+save(sentiments, file = "assets/ingested/labels.rda")
+
+print("Data was ingested and saved successfully")
+
+# print(head(filtered_tokens, 3))
+
+# print(comment)
+# print(comment_without_stop_words)
